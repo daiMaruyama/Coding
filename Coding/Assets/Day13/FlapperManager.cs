c5879680d4f5af2f556fa41275d90e9aaa47747a
mkgi16cd <mkgi16cd@gmail.com>
2025-08-06 22:33:54 +0900

    [add] Sceneをリトライする機能を追加

--- Coding/Assets/Day13/FlapperManager.cs
+++ Coding/Assets/Day13/FlapperManager.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FlapperManager : MonoBehaviour
 {
-    [SerializeField] GameObject _gameOverPanel;
-    
+    [SerializeField] GameObject _gameOverText;
+    [SerializeField] GameObject _retryText;
+    bool _isGameOver = false;
+
+    private void Update()
+    {
+        if (_isGameOver && Input.GetKeyDown(KeyCode.Return))
+        {
+            string nowScene = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(nowScene);
+        }
+    }
+
     public void GameOver()
     { 
-        _gameOverPanel.SetActive(true);
+        _gameOverText.SetActive(true);
+        _retryText.SetActive(true);
+        _isGameOver = true;
     }
 }
