5afd664b3fb45389af6c4545e1b85089165daf2c
mkgi16cd <mkgi16cd@gmail.com>
2025-08-07 23:22:22 +0900

    [fix] GameOver条件を修正

--- Coding/Assets/Day13/BounceDuck.cs
+++ Coding/Assets/Day13/BounceDuck.cs
@@ -8,6 +8,7 @@ public class BounceDuck : MonoBehaviour
     [SerializeField] float _jumpPower = 5.0f;
     [SerializeField] ScoreDuck _score;
     [SerializeField] FlapperManager flapperManager;
+    bool _isDead = false;
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -15,18 +16,31 @@ public class BounceDuck : MonoBehaviour
 
     void Update()
     {
+        if (_isDead) return;
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rb.velocity = Vector3.up * _jumpPower;
         }
+        if (transform.position.y >= 14 || transform.position.y <= -14 )
+        {
+            ShowGameOver();
+        }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    void ShowGameOver()
     {
         flapperManager.GameOver();
+        _isDead = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ShowGameOver();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (_isDead) return;
         _score.AddScore(1);
     }
 }
